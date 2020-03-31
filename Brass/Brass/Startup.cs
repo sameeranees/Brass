581054947f372c9aa3b50e1201fb13cf98768966
sameeranees <sj02732@st.habib.edu.pk>
2020-03-31 16:55:04 +0500

    Created Project and made Login and Signup Pages

---
+++ Brass/Brass/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Brass.Startup))]
+namespace Brass
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
